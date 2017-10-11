023133ba419d2ce2f2932e690bc974103e61cb6b
Reshma Patel <reshma1529@gmail.com>
2017-10-11 18:20:56 +0200

    try to create route for cook

--- routes/cooks.js
+++ routes/cooks.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
+const {ensureLoggedIn} = require('../middlewares/auth');
 
 router.get("/", (req, res, next) => {
   User.find(
@@ -16,6 +17,23 @@ router.get("/", (req, res, next) => {
     }
   );
 });
+
+router.get('/.id', ensureLoggedIn, (res, req, next) => {
+      User.findOne({
+          _id: req.params.id,
+          isCook: true,
+        },
+        (err, cook) => {
+          if (err || !cook) {
+            req.flash('error', `There's no cook available with id ${req.params.id}`);
+            return res.redirect('/cooks');
+          }
+          res.render('cook-profile', {
+            cook: cook,
+          });
+        }
+      );
+        })
 
 
 module.exports = router;
